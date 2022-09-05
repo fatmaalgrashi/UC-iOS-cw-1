d074d4d7ab643bac7d5c07e536442f7c0eb63628
Fatima AlGarashi <63903553+fatmaalgrashi@users.noreply.github.com>
2022-09-06 00:25:36 +0300

    CWdraft

---
+++ CWdraft/CWdraft/CWdraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWdraftApp.swift
+//  CWdraft
+//
+//  Created by Fatima Garashi on 06/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CWdraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CWdraft/CWdraft/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  CWdraft
+//
+//  Created by Fatima Garashi on 06/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    
+                                                
+                                                                
+            VStack{
+                
+                Text("Hello, world!")
+                    .padding()
+                HStack{
+                    Image(systemName: "heart")
+                    Image(systemName: "gear")
+                }
+            }
+        
+       
+        
+       
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
